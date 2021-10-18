8cfb87f0a1d9abc43e6f615cfd6ac65f5f8af182
Michael Kao <bttb2018to2019@gmail.com>
2021-10-18 23:54:06 +0800

    feat: 新增排程 StartOrder
    作加總邏輯

--- go/layout/internal/modules/order/order_use_case.go
+++ go/layout/internal/modules/order/order_use_case.go
@@ -2,6 +2,11 @@ package order
 
 import (
 	"context"
+	"fmt"
+	"runtime/debug"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -16,6 +21,8 @@ type useCaseOrder interface {
 	GetOrderInfo(mongoRS *mongo.Client, account string) (orderData, error)
 
 	DoSyncRecord(ctxStopNotify context.Context)
+
+	DoSyncRecordForSchedule(ctxStopNotify context.Context, goCount int)
 }
 
 type order struct {
@@ -39,4 +46,61 @@ func (o *order) DoSyncRecord(ctxStopNotify context.Context) {
 			return
 		}
 	}
+}
+
+func (o *order) DoSyncRecordForSchedule(ctxStopNotify context.Context, goCount int) {
+	taskData := o.producer(ctxStopNotify, goCount)
+	o.consumer(taskData)
+}
+
+func (o *order) producer(ctxStopNotify context.Context, goCount int) <-chan int {
+	data := make(chan int, 1)
+
+	go func(ctxStopNotify context.Context, data chan<- int, goCount int) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.WithFields(log.Fields{
+					"err": err,
+				}).Panic("producer")
+
+				debug.PrintStack()
+			}
+		}()
+
+		defer close(data)
+
+		for i := 0; i < goCount; i++ {
+			select {
+			case <-ctxStopNotify.Done():
+				return
+			default:
+				sum := i + i
+				data <- sum
+			}
+		}
+	}(ctxStopNotify, data, goCount)
+
+	return data
+}
+
+func (o *order) consumer(data <-chan int) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.WithFields(log.Fields{
+				"err": err,
+			}).Panic("consumer")
+
+			debug.PrintStack()
+		}
+	}()
+
+	total := 0
+
+	for v := range data {
+		time.Sleep(3 * time.Second)
+
+		total += v
+	}
+
+	fmt.Println("total: ", total)
 }
