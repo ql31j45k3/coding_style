921821fb5f86cb9b5a56ac4c8cede4a93565414b
Michael Kao <bttb2018to2019@gmail.com>
2022-10-18 17:59:14 +0800

    Add: response.Err 範例

---
+++ go/layout_2/internal/domain/student/const.go
@@ -0,0 +1,7 @@
+package student
+
+import "layout_2/internal/libs/response"
+
+var (
+	ErrNickNameIsEmpty = response.Err(response.Code100401, "nickName 是必填項目")
+)
--- go/layout_2/internal/domain/student/student.go
+++ go/layout_2/internal/domain/student/student.go
@@ -13,7 +13,8 @@ import (
 type Student struct {
 	gorm.Model
 
-	Name string `json:"name" binding:"required,min=1,max=10" gorm:"column:name; type:varchar(10); NOT NULL; DEFAULT:''; comment:姓名; index:idx_name;"`
+	Name     string `json:"name" binding:"required,min=1,max=10" gorm:"column:name; type:varchar(10); NOT NULL; DEFAULT:''; comment:姓名; index:idx_name;"`
+	NickName string `json:"nick_name" gorm:"column:nick_name; type:varchar(10); NOT NULL; DEFAULT:''; comment:暱稱;"`
 
 	Gender int `json:"gender" binding:"studentGender" gorm:"column:gender; type:TINYINT(1); NOT NULL; DEFAULT:0; comment:性別 0:男, 1:女;"`
 	Status int `json:"status" binding:"studentStatus" gorm:"column:status; type:TINYINT(1); NOT NULL; DEFAULT:1; comment:狀態 0:禁用, 1:啟用;"`
